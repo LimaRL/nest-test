0261a6a2814020b10b7d08ce3317f9d484e9e107
Rafael Lima <rafael.ariquemes@gmail.com>
2023-06-09 18:30:58 -0300

    connecting database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // develop only
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
